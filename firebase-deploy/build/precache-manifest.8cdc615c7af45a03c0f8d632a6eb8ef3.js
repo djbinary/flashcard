ddb2bf3bfccd2b380fbe8a3730546cad9aa25e38
Phantom <phantom@phantoms-mbp.home>
2020-04-12 16:53:45 +0100

    initial stuff

---
+++ firebase-deploy/build/precache-manifest.8cdc615c7af45a03c0f8d632a6eb8ef3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8e5e1a84958019c839caed069a0eb42",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1ae9bc0d1f389a83851",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "aff6a74ee3f7d0de0462",
+    "url": "/static/js/2.343f5acd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.343f5acd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e1ae9bc0d1f389a83851",
+    "url": "/static/js/main.3ffa4b19.chunk.js"
+  },
+  {
+    "revision": "9f2ee83ba650513fb1a8",
+    "url": "/static/js/runtime-main.66b42f9e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
